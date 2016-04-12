b44d9e1d5b83cb9798b2cb0f1546ba52c89b18a0
praseedm <prasi.praseed@gmail.com>
2016-04-12 23:34:56 +0530

    Initial commit

---
+++ app/src/main/java/praseed/p6c/lifecycle_test/MainActivity.java
@@ -0,0 +1,34 @@
+package praseed.p6c.lifecycle_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG="check";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.exit){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
